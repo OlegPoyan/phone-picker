ba28fde3229222c1cc3287d1e179de68df759915
Oleg Poyan <oleg.poyan.dev@gmail.com>
2018-02-10 00:53:26 -0500

    Add more hardcoded API endpoints
    Add hardcoded routes for front-enders to start working :)
    - /api/devices/ -> return all hardcoded devices (Pixel, Pixel XL)
    - /api/devices/pixel -> return Pixel
    - /api/devices/pixexl -> return Pixel Xl
    - /api/devices/compare/pixel-pixelxl -> compare two devices

--- routes/api/devices.js
+++ routes/api/devices.js
@@ -1,11 +1,25 @@
-var router = require('express').Router();
+var router = require('express').Router(),
+    fs = require('fs'),
+    path = require('path');
 
 // hardcoded json information related to Pixel XL device
-var device1 = require('./device.json');
+var device1 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'models', 'pixel.json'), 'utf8'));
+var device2 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'models', 'pixelxl.json'), 'utf8'));
 
+router.get('/', function(req, res, next) {
+  return res.json({devices: [device1, device2]})
+});
 
-router.get('/device', function(req, res, next) {
-  return res.send(device1);
+router.get('/pixel', function(req, res, next) {
+  return res.json(device1);
+});
+
+router.get('/pixelxl', function(req, res, next) {
+  return res.json(device2);
+});
+
+router.get('/compare/pixel-pixelxl', function(req, res, next) {
+  return res.json({devices: [device1, device2]});
 });
 
 module.exports = router;
