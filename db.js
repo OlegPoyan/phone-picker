b7784dbd71e88c0c96b79b7efb2eae82f6f98787
Grant Park <grantpark@Grants-MacBook-Pro.local>
2018-02-12 15:15:46 -0500

    change to db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ var mysql = require('mysql')
 
 var connection = mysql.createConnection({
   host: 'localhost',
-  user: 'grantpark',
-  password: 'Matrix1179!',
+  user: '',
+  password: '',
   database: 'PhonePicker'
 })
 
