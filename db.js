573f8a19ae2c33a8b15753fe6afb21b18a7c80f4
Grant Park <grantpark@Grants-MacBook-Pro.local>
2018-02-12 15:03:40 -0500

    Added simple database script

---
+++ db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'grantpark',
+  password: 'Matrix1179!',
+  database: 'PhonePicker'
+})
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...');
+
+  connection.query('SELECT * FROM Device', function(err, results) {
+    if (err) throw err
+    console.log(JSON.stringify(results,undefined,2));
+  });
+});
